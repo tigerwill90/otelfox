02c7f4faffcbe00a51b1233fda5877395c0de4b9
tigerwill90 <contact@sylvainmuller.ch>
2025-04-12 11:20:20 +0200

    feat: use a separate modfile for tooling

---
+++ go.tool.mod
@@ -0,0 +1,10 @@
+module tools
+
+go 1.24.2
+
+tool go.opentelemetry.io/build-tools/gotmpl
+
+require (
+	github.com/spf13/pflag v1.0.6 // indirect
+	go.opentelemetry.io/build-tools/gotmpl v0.22.0 // indirect
+)
--- internal/semconvutil/gen.go
+++ internal/semconvutil/gen.go
@@ -4,7 +4,7 @@
 package semconvutil // import "go.opentelemetry.io/contrib/instrumentation/github.com/gin-gonic/gin/otelgin/internal/semconvutil"
 
 // Generate semconvutil package:
-//go:generate go tool gotmpl --body=../shared/semconvutil/httpconv_test.go.tmpl "--data={}" --out=httpconv_test.go
-//go:generate go tool gotmpl --body=../shared/semconvutil/httpconv.go.tmpl "--data={}" --out=httpconv.go
-//go:generate go tool gotmpl --body=../shared/semconvutil/netconv_test.go.tmpl "--data={}" --out=netconv_test.go
-//go:generate go tool gotmpl --body=../shared/semconvutil/netconv.go.tmpl "--data={}" --out=netconv.go
+//go:generate go tool -modfile=../../go.tool.mod gotmpl --body=../shared/semconvutil/httpconv_test.go.tmpl "--data={}" --out=httpconv_test.go
+//go:generate go tool -modfile=../../go.tool.mod gotmpl --body=../shared/semconvutil/httpconv.go.tmpl "--data={}" --out=httpconv.go
+//go:generate go tool -modfile=../../go.tool.mod gotmpl --body=../shared/semconvutil/netconv_test.go.tmpl "--data={}" --out=netconv_test.go
+//go:generate go tool -modfile=../../go.tool.mod gotmpl --body=../shared/semconvutil/netconv.go.tmpl "--data={}" --out=netconv.go
